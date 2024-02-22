3c5953f9cc20415ee817ca3302e460b20783590f
Mikhail Malakhov <malakhv@gmail.com>
2024-02-22 15:25:08 +0300

    Introduce TTypeInfo

--- compiler/Compiler.pas
+++ compiler/Compiler.pas
@@ -1,7 +1,26 @@
 program slang;
 
-uses Os;
+uses Types, Os;
+
+var
+    Items: Array [1..5] of TTypeInfo;
+    I: Integer;
 
 begin
     WriteLn('Welcome to S-Lang compiler. ','Target OS is ', Os.getTargetOS);
+
+    Items[1] := MakeTypeInfo('Byte');
+    Items[2] := MakeTypeInfo('Integer');
+    Items[3] := MakeTypeInfo('Char');
+    Items[4] := MakeTypeInfo('String');
+    Items[5] := MakeTypeInfo('Set');
+
+    for I := Low(Items) to High(Items) do
+    begin
+        WriteLn('Name: ', Items[I].GetName());
+        WriteLn('Simple: ', Items[I].IsSimple());
+        WriteLn('Numeric: ', Items[I].IsNumeric());
+        WriteLn();
+    end;
+
 end.
---
+++ compiler/lang/Types.pas
@@ -0,0 +1,157 @@
+{------------------------------------------------------------------------------}
+{                                                                              }
+{                                S-Lang Project                                }
+{                                                                              }
+{  Copyright (C) 1996-2024 Mikhail Malakhov <malakhv@gmail.com>                }
+{                                                                              }
+{  Licensed under the Apache License, Version 2.0 (the "License"). You may     }
+{  not use this file except in compliance with the License. You may obtain     }
+{  a copy of the License at                                                    }
+{                                                                              }
+{     http://www.apache.org/licenses/LICENSE-2.0                               }
+{                                                                              }
+{  Unless required by applicable law or agreed to in writing, software         }
+{  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   }
+{  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            }
+{                                                                              }
+{  See the License for the specific language governing permissions and         }
+{  limitations under the License.                                              }
+{                                                                              }
+{------------------------------------------------------------------------------}
+
+{------------------------------------------------------------------------------}
+{ This Unit includes basic data types' definitions and common routines to      }
+{ working with it.                                                             }
+{                                                                              }
+{ Project: S-Lang                                                              }
+{ Package: Mikhan.S-Lang                                                       }
+{ Types:   TBD                                                                 }
+{                                                                              }
+{ Dependencies: No                                                             }
+{                                                                              }
+{ Created: 22.02.2024                                                          }
+{ Authors: Mikhail.Malakhov [malakhv@gmail.com|http://mikhan.me/]              }
+{------------------------------------------------------------------------------}
+
+Unit Types;                                                             { UNIT }
+
+{$MODE DELPHI}
+{$H+}
+{$T+}
+
+Interface                                                          { INTERFACE }
+
+{
+    The kinds of data types.
+}
+type
+
+    TTypeKind = (tkUnknown, tkByte, tkWord, tkInt, tkFloat, tkChar, tkString,
+        tkEnum, tkSet, tkArray, tkRecord, tkInterface, tkClass, tkMethod,
+        tkPointer);
+
+    TFloatKind = (fkSingle, fkDouble);
+    
+    TMethodKind = (mkProcedure, mkFunction, mkConstructor, mkDestructor,
+        mkClassProcedure, mkClassFunction);
+
+type
+
+    TTypeInfo = record
+        Kind: TTypeKind;
+        function GetName(): String;
+        function IsNumeric(): Boolean;
+        function IsSimple(): Boolean;
+        function IsPointer(): Boolean;
+        function ToString(): String;
+    end;
+    PTypeInfo = ^TTypeInfo;
+
+function MakeTypeInfo(AName: String): TTypeInfo;
+
+Implementation                                                { IMPLEMENTETION }
+
+{------------------------------------------------------------------------------}
+{ TTypeInfo                                                                    }
+{------------------------------------------------------------------------------}
+
+const
+    TK_NAME_BYTE    = 'Byte';
+    TK_NAME_WORD    = 'Word';
+    TK_NAME_INT     = 'Integer';
+    TK_NAME_FLOAT   = 'Float';
+
+    TK_NAME_CHAR    = 'Char';
+    TK_NAME_STRING  = 'String';
+    
+    TK_NAME_ENUM    = 'Enum';
+    TK_NAME_SET     = 'Set';
+    TK_NAME_ARRAY   = 'Array';
+    TK_NAME_RECORD  = 'Record';
+    
+    TK_NAME_POINTER = 'Pointer';
+    
+    TK_NAME_CLASS       = 'Class';
+    TK_NAME_INTERFACE   = 'Interface';
+    TK_NAME_METHOD      = 'Method';
+    
+    TK_NAME_UNKNOWN     = 'Unknown';
+
+    TYPE_KIND_NAMES: Array [TTypeKind] of String = ( TK_NAME_UNKNOWN,
+        TK_NAME_BYTE, TK_NAME_WORD, TK_NAME_INT, TK_NAME_FLOAT, TK_NAME_CHAR,
+        TK_NAME_STRING, TK_NAME_ENUM, TK_NAME_SET, TK_NAME_ARRAY,
+        TK_NAME_RECORD, TK_NAME_INTERFACE, TK_NAME_CLASS, TK_NAME_METHOD,
+        TK_NAME_POINTER
+    );
+
+function TypeKindToStr(AKind: TTypeKind): String;
+begin
+    Result := TYPE_KIND_NAMES[AKind];
+end;
+
+function StrToTypeKind(AValue: String): TTypeKind;
+var I: TTypeKind;
+begin
+    for I := Low(TYPE_KIND_NAMES) to High(TYPE_KIND_NAMES) do
+        if TYPE_KIND_NAMES[I] = AValue then
+        begin
+            Result := I; Exit;
+        end;
+    Result := tkUnknown;
+end;
+
+function TTypeInfo.GetName(): String;
+begin
+    Result := TypeKindToStr(Self.Kind);
+end;
+
+function TTypeInfo.IsNumeric(): Boolean;
+begin
+    Result := (Self.Kind > tkUnknown) and (Self.Kind <= tkFloat);
+end;
+
+function TTypeInfo.IsSimple(): Boolean;
+begin
+    Result := (Self.Kind > tkUnknown) and (Self.Kind <= tkChar);
+end;
+
+function TTypeInfo.IsPointer(): Boolean;
+begin
+    Result := Self.Kind = tkPointer;
+end;
+
+function TTypeInfo.ToString(): String;
+begin
+    Result := Self.GetName();
+end;
+
+{------------------------------------------------------------------------------}
+{ Common                                                                       }
+{------------------------------------------------------------------------------}
+
+function MakeTypeInfo(AName: String): TTypeInfo;
+begin
+    Result.Kind := StrToTypeKind(AName);
+end;
+
+End.                                                                     { END }
