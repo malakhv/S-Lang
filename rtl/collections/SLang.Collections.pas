f8539089195461f3265b7f53b9a76120a34abcd1
Mikhail Malakhov <malakhv@gmail.com>
2024-03-23 17:49:33 +0300

    Refactoring RTL Collections

--- rtl/collections/SLang.Collections.pas
+++ rtl/collections/SLang.Collections.pas
@@ -188,53 +188,9 @@ type
         // See https://docwiki.embarcadero.com/RADStudio/Alexandria/en/Expressions_(Delphi)#Set_Operators
     end;
 
-type
-
-    {
-        The basic node with a value for all collections.
-    }
-    TNode<V> = class(TObject)
-    private
-        FValue: V;
-    protected
-        function GetValue(): V; virtual;
-        procedure SetValue(AValue: V); virtual;
-    public
-        property Value: V read GetValue write SetValue;
-        function HasValue(): Boolean;
-        constructor Create(AValue: V); virtual;
-    end;
-
 IMPLEMENTATION                                                { IMPLEMENTATION }
 
-{------------------------------------------------------------------------------}
-{ Common Stuff                                                                 }
-{------------------------------------------------------------------------------}
-
-{------------------------------------------------------------------------------}
-{ TNode                                                                        }
-{------------------------------------------------------------------------------}
-
-constructor TNode<V>.Create(AValue: V);
-begin
-    inherited Create();
-    Self.Value := AValue;
-end;
-
-function TNode<V>.GetValue(): V;
-begin
-    Result := FValue;
-end;
-
-procedure TNode<V>.SetValue(AValue: V);
-begin
-    FValue := V;
-end;
-
-function TNode<V>.HasValue(): Boolean;
-begin
-    Result := Self.Value <> nil;
-end;
+{ EMPTY }
 
 END.                                                                     { END }
 
