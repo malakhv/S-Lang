e44479f357f10b895116f8c0cd4468c5155eb17b
Mikhail Malakhov <malakhv@gmail.com>
2024-06-07 18:23:52 +0300

    Refactoring RTL Collections

--- rtl/collections/SLang.Tree.Next.pas
+++ rtl/collections/SLang.Tree.Next.pas
@@ -20,17 +20,43 @@
 {------------------------------------------------------------------------------}
 
 {------------------------------------------------------------------------------}
-{ This Unit includes basic data types' definitions and common routines to      }
-{ working with it.                                                             }
+{ This Unit contains definitions of data types and some methods to working     }
+{ with trees.                                                                  }
 {                                                                              }
 { Project: S-Lang RTL                                                          }
 { Package: S-Lang.Collections                                                  }
-{ Types:   Tree                                                                }
+{ Types:   TTreeNode, TTree                                                    }
 {                                                                              }
-{ Dependencies: No                                                             }
+{ Dependencies: SLang.Collections, SLang.List                                  }
 {                                                                              }
 { Created: 05.06.2024                                                          }
 { Authors: Mikhail.Malakhov [malakhv@gmail.com|http://mikhan.me/]              }
+{------------------------------------------------------------------------------}
+
+{------------------------------------------------------------------------------}
+{                                  Overview                                    }
+{                                                                              }
+{ This module, being part of the standard library, provides basic features for }
+{ working with trees - a widely used abstract data type that represents a      }
+{ hierarchical tree structure with a set of connected nodes.                   }
+{                                                                              }
+{ Each node in the tree can be connected to many children (depending on the    }
+{ type of tree), but must be connected to exactly one parent, except for the   }
+{ root node, which has no parent (i.e., the root node as the top-most node in  }
+{ the tree hierarchy). You could see more details about tree data structure    }                                                         }
+{ here: https://en.wikipedia.org/wiki/Tree_(data_structure)                    }
+{------------------------------------------------------------------------------}
+
+{------------------------------------------------------------------------------}
+{                                 Definitions                                  }
+{                                                                              }
+{ Ancestor  - A node reachable by repeated proceeding from child to parent.    }
+{                                                                              }
+{ Degree    - For a given node, its number of children. A leaf, by definition, }
+{             has degree zero.                                                 }
+{                                                                              }
+{ Distance  - The number of edges along the shortest path between two nodes.   }
+{                                                                              }
 {------------------------------------------------------------------------------}
 
 UNIT SLang.Tree.Next;                                                   { UNIT }
@@ -46,9 +72,7 @@ uses SLang.System, SLang.Classes, SLang.Collections, SLang.List;
 type
 
     {
-        The node of tree with specified element. For more details about tree
-        data structure, please see
-        https://en.wikipedia.org/wiki/Tree_(data_structure).
+        The node of tree with specified element.
     }
     TTreeNode = class (TCollectionItem)
     private
@@ -66,6 +90,10 @@ type
         property ChildCount: Integer read GetChildCount;
         { The list of child nodes of this tree node. }
         property Children[Index: Integer]: TTreeNode read GetChild; default;
+        { Returns True, if this tree node has no child nodes. }
+        function IsLeaf(): Boolean;
+        { Returns True, if this tree node is root node in its collection. }
+        function IsRoot(): Boolean;
         { Constructs a new TTreeNode instance. }
         constructor Create(); virtual;
         { Free all related resources. }
@@ -102,6 +130,16 @@ end;
 function TTreeNode.GetChildCount(): Integer;
 begin
     Result := FChildren.Count;
+end;
+
+function TTreeNode.IsLeaf(): Boolean;
+begin
+    Result := Self.ChildCount = 0;
+end;
+
+function TTreeNode.IsRoot(): Boolean;
+begin
+    Result := Self.Parent = nil;
 end;
 
 END.                                                                     { END }
