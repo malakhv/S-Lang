2b29f9227e06ad70596a84886d0592e08604b384
Mikhail Malakhov <malakhv@gmail.com>
2025-01-18 17:23:13 +0300

    RTL: Added empty Time unit

---
+++ rtl/time/MyUnit.pas
@@ -0,0 +1,54 @@
+{------------------------------------------------------------------------------}
+{                                                                              }
+{                                S-Lang Project                                }
+{                                                                              }
+{  Copyright (C) 1996-2024 Mikhail Malakhov <malakhv@gmail.com>                }
+{                                                                              }
+{  Licensed under the Apache License, Version 2.0 (the "License"). You may     }
+{  not use this file except in compliance with the License. You may obtain     }
+{  a copy of the License at                                                    }
+{                                                                              }
+{     http://www.apache.org/licenses/LICENSE-2.0                               }
+{                                                                              }
+{  Unless required by applicable law or agreed to in writing, software         }
+{  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT   }
+{  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.            }
+{                                                                              }
+{  See the License for the specific language governing permissions and         }
+{  limitations under the License.                                              }
+{                                                                              }
+{------------------------------------------------------------------------------}
+
+{------------------------------------------------------------------------------}
+{ This Unit includes common routines to working with date and time.            }
+{                                                                              }
+{ Project: S-Lang RTL                                                          }
+{ Package: SLang                                                               }
+{                                                                              }
+{ Dependencies: SLang.System                                                   }
+{                                                                              }
+{ Created: 08.01.2025                                                          }
+{ Authors: Mikhail.Malakhov [malakhv@gmail.com|http://mikhan.me/]              }
+{------------------------------------------------------------------------------}
+
+UNIT SLang.Time                                                         { UNIT }
+
+{$MODE DELPHI}
+{$H+}
+{$T+}
+
+INTERFACE                                                          { INTERFACE }
+
+uses SLang.System;
+
+{------------------------------------------------------------------------------}
+
+IMPLEMENTATION                                                { IMPLEMENTATION }
+
+{------------------------------------------------------------------------------}
+{ Common                                                                       }
+{------------------------------------------------------------------------------}
+
+END.                                                                     { END }
+
+{------------------------------------------------------------------------------}
